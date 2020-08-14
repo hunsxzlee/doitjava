7ff4832e20f5ce04c644a942fe4a7fddfa2b4303
hunsxz <hunsxzlee@gmail.com>
2020-08-14 13:43:29 +0900

    chapter02 - 불리언 예제

---
+++ doitjava/src/chapter02/BooleanEx02.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class BooleanEx02 {
+
+	public static void main(String[] args) {
+		boolean isMarried = false;      //false와 true의 두개만 들어갈수 있음
+		System.out.println(isMarried);
+		
+	
+	
+	
+	}
+
+}
---
+++ doitjava/src/chapter02/DoudleEx2.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class DoudleEx2 {
+
+	public static void main(String[] args) {
+		double value;    //
+		value = (1.0e20 + 5) - 1.0e20;
+		System.out.println(value);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
