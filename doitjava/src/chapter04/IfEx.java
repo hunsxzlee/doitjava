c79514d2f041717f6db86afcdb3becd3482b1a9e
hunsxz <hunsxzlee@gmail.com>
2020-08-19 15:59:56 +0900

    IfEx-숫자 비교법2

---
+++ doitjava/src/chapter04/IfEx.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+public class IfEx {
+
+	public static void main(String[] args) {
+		int score = 80;
+		
+		if (score >= 90){
+					System.out.println("점수가 90보다 크거나 같습니다.");  //중괄호가 잇을경우 if전체에 영향을 받고
+					System.out.println("등급은 A입니다.");		     //중괄호가 없을경우 한줄만 if에 영향을 받음
+		} else {  //else 구문은 if문에서 false일 경우 실행되는 영역
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B입니다.");
+		}	
+			
+//		if (score < 90){
+//					System.out.println("점수가 90버다 작습니다.");
+//					System.out.println("등급은 B입니다.");
+		}	
+
+}
+
