3eac6b9d9b63129464b2b262a072bc481899e13d
hunsxz <hunsxzlee@gmail.com>
2020-08-19 15:58:26 +0900

    Assignment-숫자 계산법

---
+++ doitjava/src/chapter03/Assignment.java
@@ -0,0 +1,21 @@
+package chapter03;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		int result = 0;   //왼쪽(0)을 오른쪽(10)에 더해라 해서 나온게 예제1
+		result += 10;
+		System.out.println("result = " + result);
+		result -= 5;
+		System.out.println("result = " + result);
+		result *= 3;
+		System.out.println("result = " + result);
+		result /= 5;
+		System.out.println("result = " + result);
+		result %= 3;
+		System.out.println("result = " + result);
+		
+		
+	}
+
+}
