8df09cda5ad0ab456b11c6d9483de8f9376682cc
hunsxz <hunsxzlee@gmail.com>
2020-09-01 16:07:35 +0900

    Game-간단한 게임 코딩2

--- doitjava/src/example/Game02.java
+++ doitjava/src/example/Game02.java
@@ -1,12 +1,14 @@
 package example;
 
+import java.util.Scanner;
+
 public class Game02 {
 	public static void main(String[] args) {
-		
-		int thisMon = 4;											//인트로 기본값을 8로 선언
+		Scanner sc = new Scanner(System.in);
+		int thisMon = 4;											//인트로 기본값을 ?로 선언
 		System.out.println(thisMon + "월의 마지막 일자는 몇 일인가요?");		//시스템 표기상으로 위의 선언한 기본값에 ""가 붙어서 나옴
 		switch(thisMon) {											//스위치문은
-			case 1:													//1월 3월 4월 7월 8월 10월 12월은 끝나는 일수가 같으므로 굳이 브레이크로 부수면서 노가다 할 필요X
+			case 1:													//끝나는 일수가 같은 달은 굳이 브레이크로 부수면서 노가다 할 필요X
 			case 3:
 			case 5:
 			case 7:
@@ -16,7 +18,7 @@ public class Game02 {
 				System.out.println(thisMon + "월은 31일까지입니다.");
 				break;
 			case 2:
-				System.out.println(thisMon + "월은 29일까지입니다.");
+				System.out.println(thisMon + "월은 29일까지입니다.");		//끝나는 일수가 다른 달만 따로 입력후 브레이크로 캔슬
 				break;
 			case 4:
 			case 6:
--- doitjava/src/example/Game03.java
+++ doitjava/src/example/Game03.java
@@ -1,10 +1,16 @@
 package example;
 
+import java.util.Scanner;
+
 public class Game03 {
-
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
-
+		Scanner sc = new Scanner(System.in);
+		
+		int num = 10;
+		for(i = 2; i < num; i++);
+			for(j = 3 ) {
+				continue;
+			}
 	}
 
 }
