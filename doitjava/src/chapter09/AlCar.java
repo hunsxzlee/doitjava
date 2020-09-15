f85c73ed4647633e69f0c625a3e00caf5e9241e5
hunsxzlee <hunsxzlee@gmail.com>
2020-09-15 16:21:55 +0900

    Car-자동차 주행 연습문

---
+++ doitjava/src/chapter09/AlCar.java
@@ -0,0 +1,14 @@
+package chapter09;
+
+public class AlCar extends Car {
+	@Override
+	public void drive() {
+		System.out.println("자율 주행합니다");
+		System.out.println("자동차가 알아서 방향을 전환합니다");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("스스로 멈춥니다");
+	}
+}
